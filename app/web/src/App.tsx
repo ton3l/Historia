445490fdfda0e00cf68124719d512b0a7a7adb42
Elton <elton.felype.s.nascimento@gmail.com>
2025-09-07 13:04:09 -0300

    feat: add tailwindcss and starting to work on theme switching

--- app/web/src/App.tsx
+++ app/web/src/App.tsx
@@ -1,11 +1,14 @@
-import './App.css'
+import './styles/App.css'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='container'>
+      <div className='square' id='primary'></div>
+      <div className='square' id='secondary'></div>
+      <div className='square' id='background'></div>
+      <div className='square' id='neutral'></div>
+    </div>
   )
 }
 
