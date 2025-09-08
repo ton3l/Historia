3638d423f33f4379e468132b58042f8fab1d2386
Elton <elton.felype.s.nascimento@gmail.com>
2025-09-08 13:20:54 -0300

    feat: add tailwind v4 themes, thinking in how to switch them

--- app/web/src/App.tsx
+++ app/web/src/App.tsx
@@ -4,10 +4,10 @@ function App() {
 
   return (
     <div className='container'>
-      <div className='square' id='primary'></div>
-      <div className='square' id='secondary'></div>
-      <div className='square' id='background'></div>
-      <div className='square' id='neutral'></div>
+      <div className='square bg-primary-light dark:bg-primary-dark'></div>
+      <div className='square bg-secondary-light dark:bg-secondary-dark'></div>
+      <div className='square bg-background-light dark:bg-background-dark'></div>
+      <div className='square bg-neutral-light dark:bg-neutral-dark'></div>
     </div>
   )
 }
