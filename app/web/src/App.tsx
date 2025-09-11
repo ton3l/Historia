2e141d1489383dbe0c9b125d061dcbb21a6d8a13
Elton <elton.felype.s.nascimento@gmail.com>
2025-09-11 10:41:02 -0300

    feat: new better way to set themes in tailwind

--- app/web/src/App.tsx
+++ app/web/src/App.tsx
@@ -1,15 +1,14 @@
-import './styles/App.css'
+import './styles/App.css';
 
 function App() {
-
-  return (
-    <div className='container'>
-      <div className='square bg-primary'></div>
-      <div className='square bg-emphasis'></div>
-      <div className='square bg-default'></div>
-      <div className='square bg-neutral'></div>
-    </div>
-  )
+    return (
+        <div className="container">
+            <div className="square bg-primary"></div>
+            <div className="square bg-accent"></div>
+            <div className="square bg-background"></div>
+            <div className="square bg-neutral"></div>
+        </div>
+    );
 }
 
-export default App
+export default App;
